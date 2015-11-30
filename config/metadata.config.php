784fcbf999af35d3c18965225bd9637dbbb976d4
Martin Smeeckaert <smeeckaert@novius.fr>
2015-11-30 12:13:51 +0100

    install base

---
+++ config/metadata.config.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * NOVIUS OS - Web OS for digital communication
+ *
+ * @copyright  2011 Novius
+ * @license    GNU Affero General Public License v3 or (at your option) any later version
+ *             http://www.gnu.org/licenses/agpl-3.0.html
+ * @link       http://www.novius-os.org
+ */
+
+return array(
+    'name'       => 'Social Widgets',
+    'version'    => '1.0.0',
+    'provider'   => array(
+        'name' => 'Novius',
+    ),
+    'namespace'  => 'Novius\Social\Widget',
+    'permission' => array(),
+    'i18n_file'  => 'novius_social_widget::metadata',
+    'launchers'  => array(),
+    'icons'      => array(
+        16 => 'static/apps/novius_social_widget/img/icon/16.png',
+        32 => 'static/apps/novius_social_widget/img/icon/32.png',
+        64 => 'static/apps/novius_social_widget/img/icon/64.png',
+    ),
+);
